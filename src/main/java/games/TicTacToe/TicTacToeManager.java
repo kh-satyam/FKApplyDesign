c11b49836fb5d0a848fe60f265ce3cf02513d5e2
kh-satyam <kh.satyam09@gmail.com>
2020-01-28 15:14:31 +0530

    Added GameMode 9By9TicTacToeVHuman Incorporated above mode in TicTacToeGameManager

--- src/main/java/games/TicTacToe/TicTacToeManager.java
+++ src/main/java/games/TicTacToe/TicTacToeManager.java
@@ -2,6 +2,7 @@ package games.TicTacToe;
 
 import games.GameManager;
 import games.TicTacToe.board.TicTacToeBoard;
+import games.TicTacToe.gameModes.EnhancedTicTacToeGameMode9By9VHuman;
 import games.TicTacToe.gameModes.TicTacToeGameModeVComputer;
 import games.TicTacToe.gameModes.TicTacToeGameModeVHuman;
 import games.TicTacToe.gameModes.TicTacToeGameModes;
@@ -23,6 +24,7 @@ public class TicTacToeManager implements GameManager {
         System.out.println("");
         System.out.println("1.Play vs Computer");
         System.out.println("2.Play vs Human");
+        System.out.println("3.Custom TicTacToeMode");
         System.out.println("");
         System.out.println("Enter your choice:");
 
@@ -56,6 +58,20 @@ public class TicTacToeManager implements GameManager {
                     goFirst=false;
                 }
                 this.ticTacToeGameMode = new TicTacToeGameModeVComputer(ticTacToeBoard,goFirst,humanPlayer);
+                ticTacToeGameMode.playGame();
+            }else if(choice.equals("3")){
+                System.out.println("Enter num of horizontal TicTacToe Boards");
+                System.out.println("Enter num of vertical TicTacToe Boards");
+                String[] dimensions = bufferedReader.readLine().split(" ");
+                int rows = Integer.valueOf(dimensions[0]);
+                int columns = Integer.valueOf(dimensions[1]);
+                System.out.println("Who would like to go First : X or O");
+                System.out.println("1.X");
+                System.out.println("2.O");
+                System.out.println("");
+                choice = bufferedReader.readLine();
+                int startingPlayer = Integer.valueOf(choice);
+                this.ticTacToeGameMode = new EnhancedTicTacToeGameMode9By9VHuman(rows,columns,startingPlayer);
                 ticTacToeGameMode.playGame();
             }
 
